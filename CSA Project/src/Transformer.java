c5258c7e5a12f087927405f7dba5cd2e76c9d815
Vivian12345 <37036547+Vivian12345@users.noreply.github.com>
2025-10-29 12:30:11 -0400

    fix halt & ipl & run

--- CSA Project/src/Transformer.java
+++ CSA Project/src/Transformer.java
@@ -15,16 +15,16 @@ public class Transformer {
     public short BinaryToDecimal(char[] bin, int count) {
         short out = 0;
         for (int i = 0; i < count; i++) {
-        	System.out.println("power of 2: "+Math.pow(2, i));
-        	System.out.println("short: "+(short) (bin[count-1-i] - '0'));
-        	System.out.println("add: "+(short) (bin[i] - '0')*Math.pow(2, i));
-        	out += (short) (bin[count-1-i] - '0')*Math.pow(2, i);
+            System.out.println("power of 2: "+Math.pow(2, i));
+            System.out.println("short: "+(short) (bin[count-1-i] - '0'));
+            System.out.println("add: "+(short) (bin[i] - '0')*Math.pow(2, i));
+            out += (short) (bin[count-1-i] - '0')*Math.pow(2, i);
         }
         System.out.println("binary:"+new String(bin));
         System.out.println("decimal:"+out);
         return out;
     }
-    
+
 
     public short OctToDecimal(String oct) {
         return Short.parseShort(oct, 8);
